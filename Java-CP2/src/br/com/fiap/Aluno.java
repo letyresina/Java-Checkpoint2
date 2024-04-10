b2469e8cd2ec9c0d51174959dd47ea76bed88a61
Vitor Hugo <vitorh565.vh@gmail.com>
2024-04-10 16:10:34 -0300

    Falta revisão de codigo e objetos com parametros

--- Java-CP2/src/br/com/fiap/Aluno.java
+++ Java-CP2/src/br/com/fiap/Aluno.java
@@ -1,5 +1,65 @@
 package br.com.fiap;
 
-public class Aluno {
+import java.time.LocalDate;
+import java.time.Period;
 
+public class Aluno {
+    private int rm;
+    private String nome;
+    private LocalDate dataDeNascimento;
+
+    public Aluno(){} //Criação de constructors vazio e com passagem de parametros
+
+    public Aluno(String nome, int rm, LocalDate dataDeNascimento){
+        this.nome = nome;
+        setRm(rm); // Não coloco um this.rm = rm; porque ele precisa passar pela verificação antes de definir o rm.
+        setDataDeNascimento(dataDeNascimento); // A mesma coisa serve para a data de nascimento.
+    }
+
+    public int getRm() {
+        return rm;
+    }
+    public void setRm(int rm) {
+        try {
+            if (rm >= 97001 && rm <= 551999) {
+                this.rm = rm;
+            } else {
+                throw new Exception("RM INVÁLIDO!");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        try {
+            LocalDate anoAtual = LocalDate.now(); // Pega a data atual do sistema
+            if (dataDeNascimento.getYear() >= 1900 && dataDeNascimento.getYear() <= anoAtual.getYear()) { // Dentro da condição ele vai pegar o ano da data inserida pelo usuario e verificar se é maior que 1900 -- Novamente ele o ano da data inserida pelo usuario e verifica se é maior ou igual ao ano atual da data do sistema.
+                this.dataDeNascimento = dataDeNascimento; // Caso esteja tudo dentro da regra de negocios ele setta a data.
+            } else {
+                this.dataDeNascimento = LocalDate.parse("1900-01-01"); // Para melhor exibição, caso ele não esteja dentro dos padrões ele insere a data acima (podemos analisar e tirar)
+                System.out.println("DATA DE NASCIMENTO INVÁLIDA!");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }}
+
+        public String calculaIdadeCompleta(int anoAtual) {
+            Period periodo = Period.between(dataDeNascimento, LocalDate.now());
+            
+            String idadeCompleta = periodo.getYears() + " anos, " + periodo.getMonths() + " meses e " + periodo.getDays() + " dias";
+
+            return idadeCompleta;
+        }
 }
--- Java-CP2/src/br/com/fiap/UsaAluno.java
+++ Java-CP2/src/br/com/fiap/UsaAluno.java
@@ -1,7 +1,41 @@
 package br.com.fiap;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javax.swing.JOptionPane;
+
 public class UsaAluno {
     public static void main(String[] args) {
+        int anoAtual = LocalDate.now().getYear();
+
+        // Declaração, instanciação e exibição de dois objetos com constructor vazio.
+        Aluno vitor = new Aluno();
+        Aluno leticia = new Aluno();
+
+        vitor.setNome("Vitor Hugo Gonçalves Rodrigues");
+        vitor.setRm(97758);
+        vitor.setDataDeNascimento(LocalDate.parse("2005-03-08")); // LocalDate.parse() define uma data -- OBS: settar no formato americano, caso deseje alterar o formato use o ofPattern(dd-MM-yyyy).
         
+        //leticia.setNome(null);
+        //leticia.setRm(12345);
+        //leticia.setDataDeNascimento(LocalDate.parse("seu niver"));
+
+        JOptionPane.showMessageDialog(null, "RM: " + vitor.getRm() + " - NOME: " + vitor.getNome() + " - IDADE COMPLETA: " + vitor.calculaIdadeCompleta(anoAtual));
+        //JOptionPane.showMessageDialog(null, "RM: " + leticia.getRm() + " - NOME: " + leticia.getNome() + " - Idade completa: " + leticia.calculaIdadeCompleta(anoAtual));
+
+        //Duvidas que tenho, qual a necessidade do anoAtual aqui e no metodo calcular idade
+        // Dsicutir se acha interessante colocar no modelo convencional (dd-MM-yyyy)
+        
+        
+        // Declaração, instanciação e exibição de dois objetos com constructor com passagem de parametros.
+
     }
 }
+
+/*Exemplo de formatação de formato de data
+    LocalDate data = vitor.getDataDeNascimento();
+    DateTimeFormatter dft = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    String formatada = data.format(dft);
+    System.out.println(formatada);
+ */
